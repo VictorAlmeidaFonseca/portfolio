b8929d67d029f372bcdd53d3f8c0502bc3b864a2
VictorAlmeidaFonseca <fonseca.almeida.victor@gmail.com>
2020-04-29 22:53:44 -0300

    Reorder the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Routes from './routers'
+import Header from './componets/Header'
+import Description from './componets/Description'
 
 const App = () => {
   return (
+    <div className="main">
+    <Header />
+    <Description 
+      name="Victor Fonseca"
+      job="Software Developer" />
     <Routes />
+    </div>
     
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import Header from '../../componets/Header'
-import Description from '../../componets/Description'
 
 const Home = () => {
     return(
-        <div className="main">
-            <Header />
-            <Description 
-                name="Victor Fonseca"
-                job="Software Developer" />
-        </div>
+        <div className="test">
+                  Home
+                  </div>
     )
 }
 
